3fe782ffad63cad9e2db970f744805593f6041c2
Gyan Kapur <gkapur@myrealbox.com>
2003-12-01 02:33:56 +0000

    Import of GKAPUR/Array-Lock-0.02 from CPAN.
    gitpan-cpan-distribution: Array-Lock
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GKAPUR/Array-Lock-0.02.tar.gz
    gitpan-cpan-author:       GKAPUR
    gitpan-cpan-maturity:     released

--- Lock.pm
+++ Lock.pm
@@ -4,10 +4,10 @@ require 5.007003;
 # use warnings;
 require Exporter;
 our @ISA = qw(Exporter);
-our %EXPORT_TAGS = ( 'all' => [ qw(lock_values unlock_values lock_keys 
-  unlock_keys lock_array unlock_array) ] );
+our %EXPORT_TAGS = ( 'all' => [ qw(lock_values unlock_values lock_indexes 
+  unlock_indexes lock_array unlock_array) ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 NAME
 
@@ -15,21 +15,21 @@ Array::Lock- Subroutines to make Arrays read-only.
 
 =head1 SYNOPSIS
 
-  use Hash::Util qw(lock_keys   unlock_keys
-                    lock_value  unlock_value
-                    lock_array  unlock_array);
+  use Array::Lock qw(lock_indexes  unlock_indexes
+                    lock_value    unlock_value
+                    lock_array    unlock_array);
 
-  @array  = qw/f o o b a r/;
-  @keyset = qw/1 2 4/;
-  lock_values(@array);
-  lock_values(@array, @keyset);
-  unlock_values(@array);
+  @array   = qw/f o o b a r/;
+  @indexes = qw/1 2 4/;
+  lock_values    (@array);
+  lock_values    (@array, @indexes);
+  unlock_values  (@array);
 
-  lock_keys   (@array);
-  unlock_keys (@array);
+  lock_indexes   (@array);
+  unlock_indexes (@array);
 
-  lock_array   (@array);
-  unlock_array (@array);
+  lock_array     (@array);
+  unlock_array   (@array);
 
 =head1 DESCRIPTION
 
@@ -37,7 +37,7 @@ C<Array::Lock> contains functions to lock an array.
 
 By default C<Array::Lock> does not export anything.
 
-=head2 Restricted hashes
+=head2 Restricted arrays
 
 Perl 5.8.0 (inadvertantly for arrays?) introduces the ability to restrict
 an array to a range of indexes... No indexes outside of these can be
@@ -46,44 +46,44 @@ it cannot be deleted and the value cannot be changed.
 
 =over 4
 
-=item lock_keys
+=item lock_indexes
 
-=item unlock_keys
+=item unlock_indexxes
 
-  lock_keys(@array);
+  lock_indexes(@array);
 
 Restricts the given arrays indexes to its current amount. No more indexes
 can be added; however, the values of current indexes can be changed.
 exists() will still work, but delete() will not, as its standard behavior
 is to get rid of the current index. B<Note>: the current implementation prevents
 bless()ing while locked. Any attempt to do so will raise an exception. Of course
-you can still bless() the array before you call lock_keys() so this shouldn't be
+you can still bless() the array before you call lock_indexes() so this shouldn't be
 a problem.
 
-Right now, lock_keys does not function with a range.  However, if I get feedback that
+Right now, lock_indexes does not function with a range.  However, if I get feedback that
 sugests that a range is desired, a hack of some sort may be possible.
 
-  unlock_keys(%hash);
+  unlock_indexes(@array);
 
 Removes the restriction on the array's indexes.
 
 =cut
-sub lock_keys   (\@) { Internals::SvREADONLY @{$_[0]}, 1; }
-sub unlock_keys (\@) { Internals::SvREADONLY @{$_[0]}, 0; }
-# You cannot lock a specific key, because of shift...
-# I guess, you could lock that one key, and allow all the other
-# keys _above_ it to be usable... should I do that?
+sub lock_indexes   (\@) { Internals::SvREADONLY @{$_[0]}, 1; }
+sub unlock_indexes (\@) { Internals::SvREADONLY @{$_[0]}, 0; }
+# You cannot lock a specific index, because of shift...
+# I guess, you could lock that one index, and allow all the other
+# indexes _above_ it to be usable... should I do that?
 
 =item lock_value
 
 =item unlock_value
 
-  lock_values   (@array, @keys);
+  lock_values   (@array, @indexes);
   lock_values   (@array);
-  unlock_values (@array, @keys);
+  unlock_values (@array, @indexes);
 
-Locks and unlocks index value pairs in an array.  If no set of keys is
-specified, then all current keys are locked.
+Locks and unlocks index value pairs in an array.  If no set of indexes is
+specified, then all current indexes are locked.
 
 =cut
 
@@ -97,9 +97,9 @@ sub unlock_values (\@;@) {
   Internals::SvREADONLY $array->[$_], 0 for @indexes ? @indexes : $[.. $#{$array};
 }
 
-=item B<lock_hash>
+=item B<lock_array>
 
-=item B<unlock_hash>
+=item B<unlock_array>
 
     lock_array(@array);
 
@@ -108,21 +108,21 @@ No value can be changed, no indexes can be added or deleted.
 
     unlock_array(@array);
 
-unlock_arrray() does the opposite of lock_array().  All keys and values
-are made read/write.  All values can be changed and keys can be added
+unlock_arrray() does the opposite of lock_array().  All indexes and values
+are made read/write.  All values can be changed and indexes can be added
 and deleted.
 
 =cut
 
 sub lock_array (\@) { #You can only retrieve from the array
   my $array = shift;
-  lock_keys(@$array);
+  lock_indexes(@$array);
   lock_values(@$array);
 }
 
 sub unlock_array (\@) {
   my $array = shift;
-  unlock_keys(@$array);
+  unlock_indexes(@$array);
   unlock_values(@$array);
 }
 1;
@@ -135,7 +135,7 @@ L<Hash::Util>, L<ReadOnly>
 
 =head1 AUTHOR
 
-Gyan Kapur, E<lt>gkapur@cpan.org<gt>
+Gyan Kapur, <gkapur@cpan.org>
 
 This is really just Schwern's code, although he doesn't know it...
 
